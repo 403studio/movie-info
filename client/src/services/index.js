9874d842c48f76618d775aec0195fe045671fa92
hao.huang <hao.huang@aliyun.com>
2019-07-27 09:28:32 +0800

    添加请求和响应拦截器

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,35 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+// import { Loading } from 'element-ui'
+// import store from '../store'
 
-const request = axios.create()
+const request = axios.create({
+  headers: {
+    showLoading: true
+  }
+})
+NProgress.configure({ showSpinner: false })
+
+request.interceptors.request.use(config => {
+  // let loadingInstance = Loading.service()
+  // store.dispatch('setLoadingInstance', loadingInstance)
+  if (config.headers.showLoading) {
+    NProgress.start()
+    delete config.headers.showLoading
+  }
+  return config
+})
+request.interceptors.response.use(response => {
+  NProgress.done()
+  // let loadingInstance = store.state.loadingInstance
+  // loadingInstance.close()
+  return response
+}, function (error) {
+  NProgress.done()
+  // let loadingInstance = store.state.loadingInstance
+  // loadingInstance.close()
+  return Promise.reject(error)
+})
 
 export default request
