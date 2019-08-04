3d106e0e2cc63e2621367c1f9f5c2be5053ffa5a
hao.huang <hao.huang@aliyun.com>
2019-08-04 17:27:20 +0800

    [feat]新增列表页，将原来的列表页修改为详情页

--- client/src/router.js
+++ client/src/router.js
@@ -26,8 +26,10 @@ export default new Router({
       component: () => import('./views/layout/movie'),
       children: [
         { path: 'create', name: 'movie-create', component: () => import('./views/movie/create') },
+        { path: 'detail/:id', name: 'movie-detail', component: () => import('./views/movie/detail') },
         { path: 'list', name: 'movie-list', component: () => import('./views/movie/list') }
       ]
-    }
+    },
+    { path: '*', redirect: { name: 'movie-list' } }
   ]
 })
